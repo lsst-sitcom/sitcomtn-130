236a344a605946c36befab727fb00517e9f84428
J. Bryce Kalmbach <jbkalmbach@gmail.com>
2024-06-07 20:23:44 +0000

    Remove nbsphinx extension.

--- conf.py
+++ conf.py
@@ -2,8 +2,3 @@
 # https://documenteer.lsst.io/technotes/
 
 from documenteer.conf.technote import *  # noqa F401 F403
-
-extensions += ['nbsphinx']
-
-# Assume notebook is pre-computed; we don't have the LSST Pipelines on Travis.
-nbsphinx_execute = 'never'
